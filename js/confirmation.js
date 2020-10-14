f59e015b677fcf1c6d101cec127dc4c8609f05b7
ClaireLise-Dev <clairedemettre@gmail.com>
2020-10-14 13:50:40 +0200

    Optimisation des const page confirmation

--- js/confirmation.js
+++ js/confirmation.js
@@ -6,26 +6,14 @@ console.log(orderId);
 let totalPrice = localStorage.getItem('totalPrice');
 console.log(totalPrice);
 
-//Fonction CreateTag
-function createTag (tag, className,content, parent, attributes) {
-    const element = document.createElement (tag)
-    element.className = className
-    element.innerHTML = content
-    for (const key in attributes){
-        element.setAttribute (key, attributes [key])
-    }
-    parent.appendChild (element)
-    return element
-}
 
-// récapitulatif de votre commande
-const div_row = document.getElementById('content-top')
-const article = createTag ('article', 'jumbotron col-md-8 teddys m-3 px-0 card shadow',null,div_row,null)
-const div = createTag ('div', 'card-body', null, article, null)
-const h2 = createTag ('h2','card-title text-danger font-weight-bold text-center','Votre commande: ',div, null)
-const order = createTag ('p', 'card-text text-center', 'Numéro de commande: ' + orderId, article, null)
-const price = createTag ('p','card-text text-center', 'Montant total de votre commande: ' + totalPrice + ' €', article, null)
-const thanksMessage = createTag ('p','card-text text-center text-danger font-weight-bold', "Merci d'avoir commandé chez Oriteddies!", article, null)
+// Récapitulatif de votre commande
+const divRow = document.getElementById('content-top')
+const divCol= createTag ('div', 'jumbotron col-md-8 teddys m-3 px-0',null,divRow,null)
+const h2 = createTag ('h2','card-title text-danger font-weight-bold text-center','Récapitulatif de votre commande: ', divCol, null)
+const order = createTag ('p', 'card-text text-center', 'Numéro de commande: ' + orderId, divCol, null)
+const price = createTag ('p','card-text text-center', 'Montant total de votre commande: ' + totalPrice + ' €', divCol, null)
+const thanksMessage = createTag ('p','card-text text-center text-danger font-weight-bold', "Vos teddies seront bientôt chez vous! Merci d'avoir commandé chez Oriteddies!", divCol, null)
 
 // Efface localStorage
 localStorage.clear()
