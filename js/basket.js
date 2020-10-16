d5873cf4bb802312f54d35eeb89a0d2b847694ff
ClaireLise-Dev <clairedemettre@gmail.com>
2020-10-16 19:16:39 +0200

    Optimisation du code suite mentorat

--- js/basket.js
+++ js/basket.js
@@ -1,58 +1,49 @@
-// Récupération des données du Localstorage
+//Récupération des données du Localstorage
 let storedTeddies = JSON.parse(localStorage.getItem('addTeddy'))
-if (storedTeddies === null || storedTeddies === undefined){
+if (storedTeddies === null || storedTeddies === undefined) {
     storedTeddies = []
 }
-console.log(storedTeddies)
 
-// Appel teddiesNumber
-teddiesNumber()
-
-// Création des bases du panier
+//Création des bases du panier
 const divRow = document.getElementById('content-top')
-const divCol = createTag ('div', 'jumbotron col-md-8 teddys m-3 px-0',null,divRow,null)
-const h2 = createTag ('h2','card-title bg-danger text-light font-weight-bold text-center','Vos Teddies:',divCol, null)
+const divCol = createTag('div', 'jumbotron col-md-8 teddys m-3 px-0', null, divRow, null)
+const h2 = createTag('h2', 'card-title bg-danger text-light font-weight-bold text-center', 'Vos Teddies:', divCol, null)
 
 
 
-// Récupération du local storage
-if(storedTeddies == null || storedTeddies.length === 0){
+
+if (storedTeddies === null || storedTeddies.length === 0) {
     // si le panier est vide
-     const emptyBasket = createTag ('p', 'card-text text-center', 'Votre panier est vide', divCol, null)
-     localStorage.clear ()
+    const emptyBasket = createTag('p', 'card-text text-center', 'Votre panier est vide', divCol, null)
+    localStorage.clear()
 } else {
     // si des teddies sont présents dans le panier : récupération des teddies
-    for (var storedTeddy of storedTeddies) {
-        const listTeddies = createTag ('ul', 'list-inline', null, divCol, null)
-        const eachTeddy = createTag ('li','list-item m-2 d-flex justify-content-between',null, divCol,null)
-        const teddiesDetails = createTag ('div', null, 'x' + storedTeddy.quantity + ' ' + storedTeddy.teddyName + ", " + storedTeddy.teddyColor, eachTeddy,null)
-        const teddyPrice = createTag ('div', 'd-flex justify-content-between align-items-center', null, eachTeddy, null)
-        const price = createTag ('div',null, storedTeddy.teddyPrice * storedTeddy.quantity + '€ ', teddyPrice, null)
-    // Création bouton Supprimer
-        const deleteBtn = createTag ('button','btn btn-secondary ml-md-3 fas fa-trash-alt',null, teddyPrice,null)
-        deleteBtn.setAttribute ('data-id', storedTeddy.teddyId)
-        deleteBtn.setAttribute ('data-color', storedTeddy.teddyColor)
-    // Ecoute de l'évènement sur le boutton
-        deleteBtn.addEventListener('click' , function (e) {
-        e.preventDefault()
-            // console.log(event.target.getAttribute('data-id'))
-            // console.log(event.target.getAttribute('data-color'))
-            const storedTeddy = storedTeddies.filter(teddy =>teddy.teddyId == e.target.getAttribute('data-id') && teddy.teddyColor == event.target.getAttribute('data-color'))[0]
-            console.log(storedTeddy)
-            if (storedTeddy.quantity >= 1){
-                storedTeddy.quantity --
-                if (storedTeddy.quantity === 0){
+    for (const storedTeddy of storedTeddies) {
+        const listTeddies = createTag('ul', 'list-inline', null, divCol, null)
+        const eachTeddy = createTag('li', 'list-item m-2 d-flex justify-content-between', null, divCol, null)
+        const teddiesDetails = createTag('div', null, 'x' + storedTeddy.quantity + ' ' + storedTeddy.teddyName + ", " + storedTeddy.teddyColor, eachTeddy, null)
+        const teddyPrice = createTag('div', 'd-flex justify-content-between align-items-center', null, eachTeddy, null)
+        const price = createTag('div', null, storedTeddy.teddyPrice * storedTeddy.quantity + '€ ', teddyPrice, null)
+        // Création bouton Supprimer
+        const deleteBtn = createTag('button', 'btn btn-secondary ml-md-3 fas fa-trash-alt', null, teddyPrice, {'data-id': storedTeddy.teddyId,'data-color':storedTeddy.teddyColor })
+        // Ecoute de l'évènement sur le boutton
+        deleteBtn.addEventListener('click', function (e) {
+            e.preventDefault()
+            const storedTeddy = storedTeddies.filter(teddy => teddy.teddyId == e.target.getAttribute('data-id') && teddy.teddyColor == e.target.getAttribute('data-color'))[0]
+            if (storedTeddy.quantity >= 1) {
+                storedTeddy.quantity--
+                if (storedTeddy.quantity === 0) {
                     const index = storedTeddies.indexOf(storedTeddy)
-                    storedTeddies.splice (index, 1)
+                    storedTeddies.splice(index, 1)
                 }
             }
-        //Enregistrement du nouveau localStorage
+            //Enregistrement du nouveau localStorage
             localStorage.setItem('addTeddy', JSON.stringify(storedTeddies))
             JSON.parse(localStorage.getItem('addTeddy'))
 
             alert('Cet article a bien été supprimé !')
             window.location.href = "basket.html"
-            })
+        })
     }
 
     let calculPrice = []
@@ -63,24 +54,23 @@ if(storedTeddies == null || storedTeddies.length === 0){
 
     const reducer = (accumulator, currentValue) => accumulator + currentValue
     const totalPrice = calculPrice.reduce(reducer, 0)
-    console.log(totalPrice)
 
-    const total = createTag ('p', 'card-text text-center text-danger font-weight-bold m-3', 'Montant total du panier = '+ totalPrice + ' €', divCol, null)
+    const total = createTag('p', 'card-text text-center text-danger font-weight-bold m-3', 'Montant total du panier = ' + totalPrice + ' €', divCol, null)
 
     // Création bouton vider le panier
-    const deleteDivCol = createTag ('div','col-md-5 text-center mx-auto',null, divCol, null)
-    const deleteAll = createTag ('button','btn btn-danger','Vider le panier', deleteDivCol, null)
+    const deleteDivCol = createTag('div', 'col-md-5 text-center mx-auto', null, divCol, null)
+    const deleteAll = createTag('button', 'btn btn-danger', 'Vider le panier', deleteDivCol, null)
 
     deleteAll.addEventListener("click", function (e) {
-     e.preventDefault();
-     localStorage.removeItem('addTeddy')
-     alert('Votre panier a bien été vidé !')
-     window.location.href = "basket.html"
+        e.preventDefault();
+        localStorage.removeItem('addTeddy')
+        alert('Votre panier a bien été vidé !')
+        window.location.href = "basket.html"
     })
 
     // Création du formulaire de commande
-    const form = createTag ('form', 'card-body text-center col-md-8 m-3 mx-auto shadow', null, divCol, null)
-    const h3 = createTag ('h3','m-md-5 text-center','Merci de remplir ce formulaire pour valider votre commande', form,null)
+    const form = createTag('form', 'card-body text-center col-md-8 m-3 mx-auto shadow', null, divCol, null)
+    const h3 = createTag('h3', 'm-md-5 text-center', 'Merci de remplir ce formulaire pour valider votre commande', form, null)
 
     // Création fonctions validité prénom, nom, ville, code postal, mail
     function validName(value) {
@@ -98,159 +88,195 @@ if(storedTeddies == null || storedTeddies.length === 0){
     }
 
     // Création fonctions validité mail
-    function validMail(value){
+    function validMail(value) {
         return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)
     }
 
     // Ajout formulaire prénom
-    const formFirstName = createTag ('div', 'form-group', null, form, null)
-    const labelFirstName = createTag ('label', null, 'Votre prénom:', formFirstName,{'for':'prénom'})
-    const firstName = createTag ('input', null , null, formFirstName, {'type':'text','class':'form-control', 'name':'Prénom', 'required':'true'})
+    const formFirstName = createTag('div', 'form-group', null, form, null)
+    const labelFirstName = createTag('label', null, 'Votre prénom:', formFirstName, {
+        'for': 'prénom'
+    })
+    const firstName = createTag('input', null, null, formFirstName, {
+        'type': 'text',
+        'class': 'form-control',
+        'name': 'Prénom',
+        'required': 'true'
+    })
 
     // Vérification de la validité du prénom
     firstName.addEventListener('change', function (event) {
-        if (validName(firstName.value)) {
-        } else {
-            alert( "Aucun chiffre ou symbole n'est autorisé.")
+        if (validName(firstName.value)) {} else {
+            alert("Aucun chiffre ou symbole n'est autorisé.")
             event.preventDefault()
         }
     })
 
     // Ajout formulaire nom
-    const formLastName = createTag ('div', 'form-group', null, form, null)
-    const labelLastName = createTag ('label', null, 'Votre nom: ', formLastName, {'for':'nom'})
-    const lastName = createTag ('input', null, null, formLastName, {'type':'text', 'class':'form-control','name':'nom', 'required':'true'})
+    const formLastName = createTag('div', 'form-group', null, form, null)
+    const labelLastName = createTag('label', null, 'Votre nom: ', formLastName, {
+        'for': 'nom'
+    })
+    const lastName = createTag('input', null, null, formLastName, {
+        'type': 'text',
+        'class': 'form-control',
+        'name': 'nom',
+        'required': 'true'
+    })
 
     // Vérification validité du nom
     lastName.addEventListener('change', function (event) {
-        if (validName(lastName.value)) {
-        } else {
+        if (validName(lastName.value)) {} else {
             alert("Aucun chiffre ou symbole n'est autorisé.")
             event.preventDefault()
         }
     })
 
     // Ajout formulaire adresse
-    const formAddress = createTag ('div','form-group',null, form, null)
-    const labelAddress = createTag ('label', null, 'Votre adresse: ', formAddress, {'for':'adresse'})
-    const address = createTag ('textarea',null, null, formAddress, {'type':'text', 'class':'form-control','name':'Adresse', 'required':'true'})
+    const formAddress = createTag('div', 'form-group', null, form, null)
+    const labelAddress = createTag('label', null, 'Votre adresse: ', formAddress, {
+        'for': 'adresse'
+    })
+    const address = createTag('textarea', null, null, formAddress, {
+        'type': 'text',
+        'class': 'form-control',
+        'name': 'Adresse',
+        'required': 'true'
+    })
 
     // Vérification validité de l'adresse
     address.addEventListener('change', function (event) {
-        if (validAddress(address.value)){
-        } else {
+        if (validAddress(address.value)) {} else {
             event.preventDefault()
             alert("Aucun symbole n'est autorisé.")
         }
     })
 
     // Ajout formulaire code postal
-    const formPostalCode = createTag ('div','form-group',null, form, null)
-    const labelPostalCode = createTag ('label', null, 'Votre code postal: ', formPostalCode, {'for':'code postal'})
-    const postalCode = createTag ('input', null, null, formPostalCode, {'type':'text', 'class': 'form-control', 'name':'code postal', 'required':'true'})
- 
+    const formPostalCode = createTag('div', 'form-group', null, form, null)
+    const labelPostalCode = createTag('label', null, 'Votre code postal: ', formPostalCode, {
+        'for': 'code postal'
+    })
+    const postalCode = createTag('input', null, null, formPostalCode, {
+        'type': 'text',
+        'class': 'form-control',
+        'name': 'code postal',
+        'required': 'true'
+    })
+
     // Vérification validité du code postal
     postalCode.addEventListener('change', function (event) {
-    if (validPostalCode(postalCode.value)) {
-    } else {
-        alert("Aucune lettre ou symbole n'est autorisé.")
-        event.preventDefault()
-    }
-})
-    //ajout formulaire ville
-    const formCity = createTag ('div','form-group', null, form, null);
-    const labelCity = createTag ('label',null, 'Votre ville: ', formCity,{'for':'ville'} )
-    const city = createTag ('input', null, null, formCity, { 'type':'text', 'class': 'form-control','name':'Ville','required':'true'})
+        if (validPostalCode(postalCode.value)) {} else {
+            alert("Aucune lettre ou symbole n'est autorisé.")
+            event.preventDefault()
+        }
+    })
+    // Ajout formulaire ville
+    const formCity = createTag('div', 'form-group', null, form, null);
+    const labelCity = createTag('label', null, 'Votre ville: ', formCity, {
+        'for': 'ville'
+    })
+    const city = createTag('input', null, null, formCity, {
+        'type': 'text',
+        'class': 'form-control',
+        'name': 'Ville',
+        'required': 'true'
+    })
 
     //Vérification validité de la ville
     city.addEventListener('change', function (event) {
-        if (validName(city.value)) {
-        } else {
+        if (validName(city.value)) {} else {
             alert("Aucun chiffre ou symbole n'est autorisé.")
             event.preventDefault()
         }
     })
 
     // Ajout formulaire mail
-    const formMail = createTag ('div', 'form-group', null, form, null)
-    const labelMail = createTag ('label', null, 'Votre adresse mail: ', formMail, {'for':'email'} );
-    const mail = createTag ('input', null, null, formMail, {'type':'email', 'class':'form-control', 'name':'mail', 'required':'true'})
+    const formMail = createTag('div', 'form-group', null, form, null)
+    const labelMail = createTag('label', null, 'Votre adresse mail: ', formMail, {
+        'for': 'email'
+    });
+    const mail = createTag('input', null, null, formMail, {
+        'type': 'email',
+        'class': 'form-control',
+        'name': 'mail',
+        'required': 'true'
+    })
 
     // Vérification de la validité du mail
     mail.addEventListener("change", function (event) {
-        if (validMail(mail.value)){
-        } else {
+        if (validMail(mail.value)) {} else {
             event.preventDefault()
             alert("Veuillez saisir une adresse mail valide (exemple : abcd@mail.com).")
         }
     })
 
     // Création bouton validation de commande
-    const submit = createTag ('button','btn btn-danger','Validez votre commande', form, {'type':'submit','name':'add','id':'valid'})
+    const submit = createTag('button', 'btn btn-danger', 'Validez votre commande', form, {
+        'type': 'submit',
+        'name': 'add',
+        'id': 'valid'
+    })
 
     // Envoi des données panier + contact au serveur si le formulaire est valide
     submit.addEventListener('click', function (event) {
-        if(validName(firstName.value) && validName(lastName.value) && validAddress(address.value) && validName(city.value) && validMail(mail.value)){
-         event.preventDefault()
+        if (validName(firstName.value) && validName(lastName.value) && validAddress(address.value) && validName(city.value) && validMail(mail.value)) {
+            event.preventDefault()
 
-         // Envoi du totalPrice au localStorage
-         localStorage.setItem('totalPrice', totalPrice)
-         const storagePrice = localStorage.getItem('totalPrice')
-         console.log(storagePrice)
+            // Envoi du totalPrice au localStorage
+            localStorage.setItem('totalPrice', totalPrice)
+            const storagePrice = localStorage.getItem('totalPrice')
 
-        // Création de l'objet contact
-         let contact = {
-            firstName: firstName.value,
-            lastName: lastName.value,
-            address: address.value,
-            postalCode: postalCode.value,
-            city: city.value,
-            email: mail.value,
+            // Création de l'objet contact
+            let contact = {
+                firstName: firstName.value,
+                lastName: lastName.value,
+                address: address.value,
+                postalCode: postalCode.value,
+                city: city.value,
+                email: mail.value,
+            }
+            console.log(contact)
+
+            // Création du tableau teddies
+            let products = []
+            for (storedTeddy of storedTeddies) {
+                let productsId = storedTeddy.teddyId
+                products.push((productsId))
+            }
+
+            // Création d'un objet regroupant contact et produits
+            let send = {
+                contact,
+                products,
+            }
+
+            // Envoi des données au serveur
+            const post = async function (data) {
+                try {
+                    let response = await fetch (APIURL + 'order', {
+                        method: 'POST',
+                        body: JSON.stringify(data),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    if (response.ok) {
+                        let data = await response.json()
+                        localStorage.setItem('Order', data.orderId)
+                        window.location = 'confirmation.html'
+                        localStorage.removeItem('addTeddy')
+
+                    } else {
+                        event.preventDefault();
+                        console.error('Retour du serveur : ', response.status)
+                        alert('Erreur rencontrée : ' + response.status)
+                    }
+                } catch (error) {
+                    alert("Erreur : " + error)
+                }
+            }
+            post(send)
         }
-        console.log(contact)
-
-       // Création du tableau teddies
-       let products = []
-       for (storedTeddy of storedTeddies) {
-           let productsId = storedTeddy.teddyId
-           products.push((productsId))
-       }
-       console.log(products)
-
-       // Création d'un objet regroupant contact et produits
-       let send = {
-           contact,
-           products,
-       }
-       console.log(send)
-
-       // Envoi des données au serveur
-       const post = async function (data){
-           try {
-               let response = await fetch('http://localhost:3000/api/teddies/order', {
-                   method: 'POST',
-                   body: JSON.stringify(data),
-                   headers: {
-                       'Content-Type': 'application/json'
-                   }
-               })
-               if (response.ok) {
-                   let data = await response.json()
-                   console.log (data.orderId)
-                   localStorage.setItem('Order', data.orderId)
-                   window.location = 'confirmation.html'
-                   localStorage.removeItem('addTeddy')
-
-               } else {
-                   event.preventDefault();
-                   console.error('Retour du serveur : ', response.status)
-                   alert('Erreur rencontrée : ' + response.status)
-               }
-           } catch (error) {
-               alert("Erreur : " + error)
-           }
-       }
-       post(send)
-   }
+    })
 }
-)}
--- js/product.js
+++ js/product.js
@@ -1,79 +1,83 @@
-    // Appel teddiesNumber
-teddiesNumber()
-
     //Récupération id dans l'URL
-console.log(window.location.search)
-const urlParams = new URLSearchParams(window.location.search)
-const id = urlParams.get('id')
-console.log(id);
+    const urlParams = new URLSearchParams(window.location.search)
+    const id = urlParams.get('id')
 
-const getTeddies = async function(url) {
-    //Récupération des données du teddy sélectionné par son id
-    try {let response = await fetch(url)
-       if (response.ok) {
-           let teddy = await response.json()
-           console.log(teddy)
 
-    //Création des éléments dans le DOM
-            const divRow = document.getElementById('content-top')
-            const divCol = createTag ('div','col-md-5 m-3 px-0',null, divRow, null)
-            const card = createTag ('div', 'card shadow', null, divCol, null)
-            const image = createTag ('img','card-img-top img-responsive', null, card, {'src':teddy.imageUrl, 'alt': teddy.name})
-            const cardBody = createTag ('div', 'card-body', null, card,null)
-            const h2 = createTag ('h2','card-title', teddy.name, cardBody, null)
-            const description = createTag ('p','card-text', teddy.description, cardBody, null)
-            const price = createTag ('h3','card-text', teddy.price /100 + ' €', cardBody, null)
-            const label = createTag ('label', null, 'Personnalisez sa couleur: ', cardBody, {'for':'Choix de coueurs de '+ teddy.name})
-            const select = createTag ('select', 'form-control', null, cardBody, {'name':'Choix de couleurs de '+teddy.name});
-            const button = createTag ('button', 'btn btn-danger mt-2', 'Ajoutez'+' '+ teddy.name+ ' '+'au panier', cardBody, null)
-            
+    const getTeddies = async function (url) {
+        //Récupération des données du teddy sélectionné par son id
+        try {
+            let response = await fetch(url)
+            if (response.ok) {
+                let teddy = await response.json()
 
-    //Récupération couleurs Teddy
-        const colors = teddy.colors;
-                for (i = 0; i < colors.length; i++) {
-                const selectColors= createTag ('option', null, colors[i], select,{'value': colors[i]})
-            }
 
-    //Ecoute de l'évènement sur le boutton
-        button.addEventListener("click", function (event) {
-        event.preventDefault();
+                //Création des éléments dans le DOM
+                const divRow = document.getElementById('content-top')
+                const divCol = createTag('div', 'col-md-5 m-3 px-0', null, divRow, null)
+                const card = createTag('div', 'card shadow', null, divCol, null)
+                const image = createTag('img', 'card-img-top img-responsive', null, card, {
+                    'src': teddy.imageUrl,
+                    'alt': teddy.name
+                })
+                const cardBody = createTag('div', 'card-body', null, card, null)
+                const h2 = createTag('h2', 'card-title', teddy.name, cardBody, null)
+                const description = createTag('p', 'card-text', teddy.description, cardBody, null)
+                const price = createTag('h3', 'card-text', teddy.price / 100 + ' €', cardBody, null)
+                const label = createTag('label', null, 'Personnalisez sa couleur: ', cardBody, {
+                    'for': 'Choix de coueurs de ' + teddy.name
+                })
+                const select = createTag('select', 'form-control', null, cardBody, {
+                    'name': 'Choix de couleurs de ' + teddy.name
+                });
+                const button = createTag('button', 'btn btn-danger mt-2', 'Ajoutez' + ' ' + teddy.name + ' ' + 'au panier', cardBody, null)
 
-    //Stockage du teddy sélectionné dans le localStorage
-        let teddiesChoosen = {
-            teddyName: teddy.name,
-            teddyId: teddy._id,
-            teddyColor: select.value,
-            quantity: 1,
-            teddyPrice: teddy.price / 100,
-        };
 
-        let storedTeddies = JSON.parse(localStorage.getItem('addTeddy'));
-        if (storedTeddies === null || storedTeddies === undefined){
-            storedTeddies = []
-        }
-        const teddyColor = select.value;
-                if(storedTeddies) {
-                    const line = storedTeddies.find (teddyTmp => teddyTmp.teddyId == teddy._id && teddyColor ==  teddyTmp.teddyColor)
-
-                    console.log (line)
-                    if (line === undefined){
-                        storedTeddies.push(teddiesChoosen);
-                    }else{
-                        line.quantity++
-                    }
-                    localStorage.setItem('addTeddy', JSON.stringify(storedTeddies));
-
-                    if (window.confirm (teddy.name + " " + teddyColor + ' a bien été ajouté à votre panier! Souhaitez-vous continuer vos achats? ')) {
-                        window.location.href = "index.html";
-                    } else {
-                        window.location.href = "basket.html";
-                    }
+                //Récupération couleurs Teddy
+                const colors = teddy.colors;
+                for (const color of colors) {
+                    const selectColors = createTag('option', null, color, select, {
+                        'value': color
+                    })
                 }
-            })
-        }
-    } catch (error) {
-        alert("Erreur : " + error);
-    }
-}
 
-getTeddies (APIURL + id);
+                //Ecoute de l'évènement sur le boutton
+                button.addEventListener("click", function (event) {
+                    event.preventDefault();
+
+                    //Stockage du teddy sélectionné dans le localStorage
+                    let teddiesChoosen = {
+                        teddyName: teddy.name,
+                        teddyId: teddy._id,
+                        teddyColor: select.value,
+                        quantity: 1,
+                        teddyPrice: teddy.price / 100,
+                    };
+
+                    let storedTeddies = JSON.parse(localStorage.getItem('addTeddy'));
+                    if (storedTeddies === null || storedTeddies === undefined) {
+                        storedTeddies = []
+                    }
+                    const teddyColor = select.value;
+                    if (storedTeddies) {
+                        const line = storedTeddies.find(teddyTmp => teddyTmp.teddyId == teddy._id && teddyColor == teddyTmp.teddyColor)
+                        if (line === undefined) {
+                            storedTeddies.push(teddiesChoosen);
+                        } else {
+                            line.quantity++
+                        }
+                        localStorage.setItem('addTeddy', JSON.stringify(storedTeddies));
+
+                        if (window.confirm(teddy.name + " " + teddyColor + ' a bien été ajouté à votre panier! Souhaitez-vous continuer vos achats? ')) {
+                            window.location.href = "index.html";
+                        } else {
+                            window.location.href = "basket.html";
+                        }
+                    }
+                })
+            }
+        } catch (error) {
+            alert("Erreur : " + error);
+        }
+    }
+//Appel de la fonction getTeddies
+getTeddies(APIURL + id);
